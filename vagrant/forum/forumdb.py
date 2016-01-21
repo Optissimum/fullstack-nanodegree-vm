8208ff2a92cf8ed321981b357068c4c70eef1596
Optissimum <sebas310@gmail.com>
2016-01-21 00:52:58 -0500

    Feat: Connect GetAllPosts to forum DB

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,31 +1,22 @@
 #
 # Database access functions for the web forum.
-# 
+#
 
 import time
-
-## Database connection
-DB = []
+import psycopg2
 
 ## Get posts from database.
 def GetAllPosts():
-    '''Get all the posts from the database, sorted with the newest first.
-
-    Returns:
-      A list of dictionaries, where each dictionary has a 'content' key
-      pointing to the post content, and 'time' key pointing to the time
-      it was posted.
-    '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    conn = psycopg2.connect("dbname=forum")
+    cur = conn.cursor()
+    # Grabs all of our posts from our the DB
+    cur.execute("SELECT time, content FROM posts ORDER BY time desc")
+    # Processes our fecthall into a dicitonary
+    posts = ({'content': str(row[1]), 'time': str(row[0])}
+    for row in cur.fetchall())
+    conn.close()
     return posts
 
 ## Add a post to the database.
 def AddPost(content):
-    '''Add a new post to the database.
-
-    Args:
-      content: The text content of the new post.
-    '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    

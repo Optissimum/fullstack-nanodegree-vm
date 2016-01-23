e61bc0192acf9ca072d7680ffd0a828cc7df7376
Sebastian MacDonald <sebas310@gmail.com>
2016-01-23 03:55:50 -0500

    Create initial player and match tables

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,10 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE table players(id serial primary key, name text, birthdate date, region text, team serial, points serial);
 
+CREATE table matches(id serial primary key, playerone text, playertwo text, playerthree text);
+
+CREATE view playerpoints AS SELECT name, points FROM players ORDER BY points desc;
+
+CREATE view regionsview AS SELECT region, count(region) as num FROM players GROUP BY region ORDER BY num desc;

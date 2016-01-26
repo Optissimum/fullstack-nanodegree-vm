99faba0734c1f060af69bae27305308e8d61e789
Sebastian MacDonald <sebas310@gmail.com>
2016-01-25 22:17:34 -0500

    Feature Add playerStandings function

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -31,59 +31,22 @@ def countPlayers(region = None):
     cur.execute(query)
     return cur.fetchone()[0]
 
-
 def registerPlayer(name, birthdate = None, region = '', teamNumber = 0):
-    """Adds a player to the tournament database.
-
-    The database assigns a unique serial id number for the player.  (This
-    should be handled by your SQL database schema, not in your Python code.)
-
-    Args:
-      name: the player's full name (need not be unique).
-    """
     cur = connect().cursor()
-    query = 'INSERT INTO players (name, birthdate,region,team) VALUES (%s, %s, %s, %s,)'
+    query = 'INSERT INTO players (name, birthdate,region,team) VALUES (%s, %s, %s, %s)'
     cur.execute(query, (name, birthdate, region, teamNumber))
 
-
-
-
 def playerStandings():
-    """Returns a list of the players and their win records, sorted by wins.
-
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
-
-    Returns:
-      A list of tuples, each of which contains (id, name, wins, matches):
-        id: the player's unique id (assigned by the database)
-        name: the player's full name (as registered)
-        wins: the number of matches the player has won
-        matches: the number of matches the player has played
-    """
-
+    #Returns a list of the players and their win records, sorted by wins.
+    cur = connect().cursor()
+    cur.execute('SELECT * FROM playerpoints')
+    standings = ({'id' str(row[0]), 'name': str(row[1]), 'wins': str(row[2]), 'matches': str(row[3])) }
+        for row in cur.fetchall())
 
 def reportMatch(winner, loser):
-    """Records the outcome of a single match between two players.
-
-    Args:
-      winner:  the id number of the player who won
-      loser:  the id number of the player who lost
-    """
-
+    #Records the outcome of a single match between two players.
 
 def swissPairings():
-    """Returns a list of pairs of players for the next round of a match.
-
-    Assuming that there are an even number of players registered, each player
-    appears exactly once in the pairings.  Each player is paired with another
-    player with an equal or nearly-equal win record, that is, a player adjacent
-    to him or her in the standings.
-
-    Returns:
-      A list of tuples, each of which contains (id1, name1, id2, name2)
-        id1: the first player's unique id
-        name1: the first player's name
-        id2: the second player's unique id
-        name2: the second player's name
-    """
+    #Returns a list of pairs of players for the next round of a match.
+    #Sort desc, group in 2's which allows for an easy "pairing"
+    #Return a list of tuples, each of which contains (id1, name1, id2, name2)

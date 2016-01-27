df12702b61b5ef31ce83cc6a3029b1888a082a2b
Sebastian MacDonald <sebas310@gmail.com>
2016-01-27 07:52:18 -0500

    Fix various bugs in tournament.py Correctly close db connections when we're done with it in each func Clean up playerStandings so we're correctly making the list-comp the tester script is asking for

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -6,47 +6,58 @@
 import psycopg2
 import bleach
 
-def connect(close = False):
-    conn = psycopg2.connect("dbname=tournament") #Connect to database
-    #Close that thang if they request it, otherwise open another one up
-    #psycopg2.close() if close
-    return conn
+def connect():
+    return psycopg2.connect("dbname=tournament") #Connect to database
 
 def deleteMatches():
-    """Remove all the match records from the database."""
-    cur = connect().cursor()
-    query = 'DELETE FROM matches'
+    #Remove all the match records from the database.
+    conn = connect()
+    cur = conn.cursor()
+    query = 'DELETE FROM matches;'
     cur.execute(query)
+    conn.commit()
+    cur.close()
 
 def deletePlayers():
-    """Remove all the player records from the database."""
-    cur = connect().cursor()
-    query = 'DELETE FROM players'
+    #Remove all the player records from the database.
+    conn = connect()
+    cur = conn.cursor()
+    query = 'DELETE FROM players;'
     cur.execute(query)
+    conn.commit()
+    conn.close()
 
 def countPlayers(region = None):
-    cur = connect().cursor()
+    conn = connect()
+    cur = conn.cursor()
     region = bleach.clean(region)
-    query = "SELECT count(name) AS num FROM players"
-    cur.execute(query)
-    return cur.fetchone()[0]
+    cur.execute('SELECT count(name) AS num FROM players;')
+    num = cur.fetchone()[0]
+    conn.close()
+    return num
 
 def registerPlayer(name, birthdate = None, region = '', teamNumber = 0):
-    cur = connect().cursor()
-    query = 'INSERT INTO players (name, birthdate,region,team) VALUES (%s, %s, %s, %s)'
+    conn = connect()
+    cur = conn.cursor()
+    query = "INSERT INTO players (name, birthdate, region, team) VALUES (%s, %s, %s, %s);"
     cur.execute(query, (name, birthdate, region, teamNumber))
+    conn.commit()
+    conn.close()
 
 def playerStandings():
     #Returns a list of the players and their win records, sorted by wins.
-    cur = connect().cursor()
-    cur.execute('SELECT * FROM playerpoints')
-    standings = ({'id' str(row[0]), 'name': str(row[1]), 'wins': str(row[2]), 'matches': str(row[3])) }
-        for row in cur.fetchall())
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM playerpoints;')
+    standings = [(row[0], row[1], row[2], row[3]) for row in cur.fetchall()]
+    conn.close()
+    return standings
 
-def reportMatch(winner, loser):
+#def reportMatch(winner, loser):
     #Records the outcome of a single match between two players.
 
-def swissPairings():
+
+#def swissPairings():
     #Returns a list of pairs of players for the next round of a match.
     #Sort desc, group in 2's which allows for an easy "pairing"
     #Return a list of tuples, each of which contains (id1, name1, id2, name2)

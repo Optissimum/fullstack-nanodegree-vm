ddbf5d8e40bee8bcf1e17a4096c430a55e2c759d
Optissimum <sebas310@gmail.com>
2016-01-29 10:33:35 -0500

    Update: Change formatting and some variable names for clarity

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -13,18 +13,19 @@ CREATE table players(
   name text,
   birthdate date,
   tourney text,
-  matches integer default 0,
+  rounds integer default 0,
   points integer default 0);
 
 CREATE table matches(
   id serial primary key,
-  pone serial references players(id),
   ptwo serial references players(id),
+  pone serial references players(id),
   tourney text,
-  winner text);
+  winner integer);
 
-CREATE view playerpoints AS SELECT id, name, points, matches FROM players ORDER BY points desc;
+CREATE view playerpoints
+  AS SELECT id, name, points, rounds
+  FROM players
+  ORDER BY points desc;
 
 CREATE view tourneyview AS SELECT tourney, count(tourney) as num FROM players GROUP BY tourney ORDER BY num desc;
-
-CREATE view twomatch AS SELECT id, pone, ptwo, tourney FROM matches;
